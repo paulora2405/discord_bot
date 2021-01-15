9c06c4e5bdf5dbc29fc6146c9a68cb7d7382e2bf
paulora2405 <paulora2405@gmail.com>
2021-01-15 19:01:57 -0300

    Deleted no-arg case, which is treated on index.js

--- commands/gif.js
+++ commands/gif.js
@@ -11,13 +11,12 @@ module.exports = {
     if (msg.mentions.size)
       return msg.channel.send('Não precisa marca ngm burro! Só coloca um termo pra pesquisa caralho')
 
-    if (args.length == 0)
-      return msg.reply(`Bota um termo para pesquisa ai porra`);
-    else if (args.length > 1) {
+
+    if (args.length > 1) {
       termo = args.join('-');
     }
     else {
-      termo = args.pop();
+      termo = args.first;
     }
 
     let url = `https://api.tenor.com/v1/search?key=${process.env.TENOR_KEY}&q=${termo}&locale=pt_BR&contentFilter=off&media_filter=basic&ar_range=standard&limit=10`
