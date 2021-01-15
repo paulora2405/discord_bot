31e85a1adb5168b365b08c9183df6656568968cc
paulora2405 <paulora2405@gmail.com>
2021-01-15 19:02:49 -0300

    Field 'args' assined true

--- commands/eh?.js
+++ commands/eh?.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'eh?',
   description: 'Confirma se a pessoa marcada é',
-  args: false,
+  args: true,
   usage: '<pessoa>',
   aliases: ['eh', 'é', 'é?'],
   execute(msg, args) {
