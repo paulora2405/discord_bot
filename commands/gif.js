f991c6a49ed462d378a2a32eda724833c46be821
paulora2405 <paulora2405@gmail.com>
2021-01-17 04:20:34 -0300

    Implemented the giphy api for search

--- commands/gif.js
+++ commands/gif.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const { using_giphy } = require('../config.json');
 
 module.exports = {
   name: 'gif',
@@ -16,13 +17,26 @@ module.exports = {
       termo = args.join('-');
     }
     else {
-      termo = args.first;
+      termo = args.shift();
     }
 
-    let url = `https://api.tenor.com/v1/search?key=${process.env.TENOR_KEY}&q=${termo}&locale=pt_BR&contentFilter=off&media_filter=basic&ar_range=standard&limit=10`
-    let response = await fetch(url);
-    let json = await response.json();
-    // console.log(json);
-    return msg.channel.send(json.results[Math.floor(Math.random() * json.results.length)].url);
+    console.log(using_giphy);
+
+    if (using_giphy) {
+      let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_KEY}&q=${termo}&limit=10&lang=pt`;
+      let response = await fetch(url);
+      let json = await response.json();
+      // console.log(url);
+      let index = Math.floor(Math.random() * json.data.length);
+      return msg.channel.send(json.data[index].url);
+    }
+    else {
+      let url = `https://api.tenor.com/v1/search?key=${process.env.TENOR_KEY}&q=${termo}&locale=pt_BR&contentFilter=off&media_filter=basic&ar_range=standard&limit=10`
+      let response = await fetch(url);
+      let json = await response.json();
+      // console.log(url);
+      let index = Math.floor(Math.random() * json.results.length);
+      return msg.channel.send(json.results[index].url);
+    }
   }
 }
