c93ebbd4a6cfbf9a66ce510fee709ac0662d0359
paulora2405 <paulora2405@gmail.com>
2021-01-17 22:55:13 -0300

    Started implementing guild database

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 
+const Datastore = require('nedb');
 const { prefix } = require('./config.json');
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+
+const guildsDatabase = new Datastore({
+  filename: `databases/guildsPrefixes.db`,
+  corruptAlertThreshold: 0,
+  autoload: true
+});
+
 
 console.log('Inicializando o bot...');
 
@@ -13,8 +21,8 @@ client.once('ready', () => {
   client.user.setPresence({
     status: "online",
     activity: {
-      name: "tu fazendo merda",
-      type: "WATCHING"
+      type: "WATCHING",
+      name: "tu fazendo merda"
     }
   }).catch(console.error);
 });
@@ -31,6 +39,14 @@ client.on('message', msg => {
   // se a mensagem nao começa com o prefixo ou o autor da mensagem é um bot, nao faz nada
   if (!msg.content.startsWith(prefix) || msg.author.bot)
     return;
+
+  // guildsDatabase.insert({
+  //   guild_name: msg.guild.name,
+  //   guild_id: msg.guild.id,
+  //   guild_prefix: prefix
+  // }, err => {
+  //   console.error(`Ocorreu um erro ao inserir o servidor ${msg.guild.name} (id=${msg.guild.id})`, err);
+  // });
   // cria uma array de strings contendo todos os argumentos passados com o comando
   const args = msg.content.slice(prefix.length).trim().split(/ +/);
   // o comando em si, sem o prefixo, somente com minusculas
