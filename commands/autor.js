2ddcf8cab1b827516ff514d6e70c626dbd180783
paulora2405 <paulora2405@gmail.com>
2021-01-31 00:12:03 -0300

    Added more author information

--- commands/autor.js
+++ commands/autor.js
@@ -7,6 +7,10 @@ module.exports = {
   aliases: ['criador'],
   execute(msg, args, prefix = defaultPrefix) {
     msg.react('💯').catch(err => console.error(`Não consegui reagir a mensagem de ${msg.author.tag}.\n`, err));
-    return msg.channel.send(`O meu autor é o <@323098525766385665>!`);
+    const data = [];
+    data.push(`O meu autor é o <@323098525766385665>!`);
+    data.push(`Código fonte do Bot em:`);
+    data.push(`https://github.com/paulora2405/discord_bot`);
+    return msg.channel.send(data);
   }
 }
